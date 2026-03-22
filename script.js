d6873466b5431c221e270ac23f4d888e0bd719d5
prabhat-bably1 <prabhatr.seth04@gmail.com>
2026-03-22 22:51:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-let currentPage = 0;
-const pages = document.querySelectorAll(".page");
-
-function nextPage() {
-    if (currentPage < pages.length) {
-        pages[currentPage].classList.remove("active");
-        currentPage++;
-    }
+function flipBook() {
+    document.getElementById("bookBox").classList.toggle("flip");
 }
