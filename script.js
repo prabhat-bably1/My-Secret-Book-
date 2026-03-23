86972bac5a25fee4d0a8e51910ff74b6ec4b25a5
prabhat-bably1 <prabhatr.seth04@gmail.com>
2026-03-23 15:45:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-function nextPage() {
-    document.getElementById("page2").innerHTML = `
-        <h3>Welcome to your Secret World ✨</h3>
-        <p>
-        Start your journey here... <br>
-        Share your thoughts, jokes, shayari and feelings 💛
-        </p>
-    `;
+function openPage() {
+    document.getElementById("page").style.display = "block";
 }
